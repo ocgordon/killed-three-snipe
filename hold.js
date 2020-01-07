c408c6dd2fb762905b60247ff1431ea9c25b76bf
ocgordon <ocgordon@github.com>
2020-01-07 16:08:28 -0500

    feat: add mutationobserver

--- hold.js
+++ hold.js
@@ -1,3 +1,18 @@
+// {
+//   "manifest_version": 2,
+//   "name": "Killed three snipe",
+//   "description": "And so did everyone else",
+//   "version": "1.0",
+//   "permissions": ["https://twitter.com/*"],
+//   "content_scripts": [
+//     {
+//       "matches": ["*://*/*"],
+//       "js": ["content.js"],
+//       "run_at": "document_end"
+//     }
+//   ]
+// }
+
 // Get tweets from feed.
 var tweets = document.getElementsByClassName(
   'css-901oao css-16my406 r-gwet1z r-ad9z0x r-bcqeeo r-qvutc0',
@@ -5,8 +20,8 @@ var tweets = document.getElementsByClassName(
 
 console.log(tweets);
 
-// Iterate through tweets and add snipe murders.
 function killSnipe() {
+  // Iterate through tweets and add snipe murders.
   for (var i = 0; i < tweets.length; i++) {
     var tweet = tweets[i];
     console.log('Made it to the loop!');
