2c93ddc02018da5a3fa32ba86447e14148e91cee
ocgordon <ocgordon@github.com>
2020-01-07 16:50:33 -0500

    feat: better now

--- content.js
+++ content.js
@@ -1,28 +1,36 @@
-// Get tweets from feed.
-var tweets = document.getElementsByClassName(
-  'css-901oao css-16my406 r-gwet1z r-ad9z0x r-bcqeeo r-qvutc0',
-);
+//Ensure DOM is loaded.
+document.addEventListener('DOMContentLoaded', function() {
+  // Get tweets from feed.
+  var tweets = document.getElementsByClassName(
+    'css-901oao css-16my406 r-gwet1z r-ad9z0x r-bcqeeo r-qvutc0',
+  );
 
-console.log(tweets);
+  console.log(tweets);
+  console.log(tweets.length);
 
-// Iterate through tweets and add snipe murders.
-function killSnipe() {
-  for (var i = 0; i < tweets.length; i++) {
-    var tweet = tweets[i];
-    console.log('Made it to the loop!');
-    tweets[i].innerHTML = tweets[i].innerHTML + 'Killed three snipe.';
+  // Iterate through tweets and add snipe murders.
+  function killSnipe() {
+    for (var i = 0; i < tweets.length; i++) {
+      var tweet = tweets[i];
+      console.log('Made it to the loop!');
+      tweet.innerHTML = tweet[i].innerHTML + 'Killed three snipe.';
+    }
   }
-}
 
-var observer = new MutationObserver(mutations => {
-  mutations.forEach(mutation => {
-    killSnipe();
+  var observer = new MutationObserver(mutations => {
+    mutations.forEach(mutation => {
+      killSnipe();
+    });
   });
+
+  const config = {
+    characterData: true,
+    subtree: true,
+  };
+
+  observer.observe(document, config);
 });
 
-const config = {
-  characterData: true,
-  subtree: true,
-};
-
-observer.observe(document, config);
+// Tweet Classes
+// css-1dbjc4n r-my5ep6 r-qklmqi r-1adg3ll
+// css-901oao css-16my406 r-gwet1z r-ad9z0x r-bcqeeo r-qvutc0
