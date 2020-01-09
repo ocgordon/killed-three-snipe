6a51176c0c5e5124667eb2292dc2bfabda9e8bac
ocgordon <ocgordon@github.com>
2020-01-09 10:39:18 -0500

    update tweets collection

--- content.js
+++ content.js
@@ -1,35 +1,42 @@
 //Ensure DOM is loaded.
-document.addEventListener('DOMContentLoaded', function() {
-  // Get tweets from feed.
-  var tweets = document.getElementsByClassName(
-    'css-901oao css-16my406 r-gwet1z r-ad9z0x r-bcqeeo r-qvutc0',
-  );
+// document.addEventListener('DOMContentLoaded', function() {
+// Get tweets from feed.
+var tweets = document.getElementsByClassName('css-1dbjc4n r-my5ep6 r-qklmqi r-1adg3ll');
 
-  console.log(tweets);
-  console.log(tweets.length);
+console.log(tweets);
 
-  // Iterate through tweets and add snipe murders.
-  function killSnipe() {
-    for (var i = 0; i < tweets.length; i++) {
-      var tweet = tweets[i];
-      console.log('Made it to the loop!');
-      tweet.innerHTML = tweet[i].innerHTML + 'Killed three snipe.';
-    }
+function collectionToArray(tweets) {
+  var i,
+    length = tweets.length,
+    tweetsArray = [];
+  for (i = 0; i < length; i++) {
+    tweetsArray.push(tweets[i]);
   }
+  return tweetsArray;
+}
 
-  var observer = new MutationObserver(mutations => {
-    mutations.forEach(mutation => {
-      killSnipe();
-    });
+// Iterate through tweets and add snipe murders.
+function killSnipe() {
+  for (var i = 0; i < tweets.length; i++) {
+    var tweet = tweets[i];
+    console.log('Made it to the loop!');
+    tweet.innerHTML = tweet.innerHTML + 'Killed three snipe.';
+  }
+}
+
+var observer = new MutationObserver(mutations => {
+  mutations.forEach(mutation => {
+    killSnipe();
   });
-
-  const config = {
-    characterData: true,
-    subtree: true,
-  };
-
-  observer.observe(document, config);
 });
+
+const config = {
+  characterData: true,
+  subtree: true,
+};
+
+observer.observe(document, config);
+// });
 
 // Tweet Classes
 // css-1dbjc4n r-my5ep6 r-qklmqi r-1adg3ll
